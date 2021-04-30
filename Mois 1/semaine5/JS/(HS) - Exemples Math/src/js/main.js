c22f177e6c50b85147efcafcd42330451489b76a
Seifyuuu <elgayas@hotmail.com>
2021-04-30 16:53:41 +0200

    notes math

---
+++ Mois 1/semaine5/JS/(HS) - Exemples Math/src/js/main.js
@@ -0,0 +1,23 @@
+let nombre = 9/5;
+console.log(nombre);
+
+
+
+
+
+
+//Math.round() arrondit vers l'entier le plus proche
+
+console.log(Math.round(nombre));
+
+//Math.ceil() arrondit vers le plus haut
+
+console.log(Math.ceil(nombre));
+
+//Math.floor() arrondit vers le plus bas 
+
+console.log(Math.floor(nombre));
+
+//Math.random génére un nombre aléatoire entre 0 et 1 
+
+console.log(Math.random())
