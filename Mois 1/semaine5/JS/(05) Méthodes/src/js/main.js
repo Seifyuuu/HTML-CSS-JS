e3f4604484fb1f0f5d46f980e5d7e46916d20a98
Seifyuuu <elgayas@hotmail.com>
2021-04-30 16:14:42 +0200

    correction

--- Mois 1/semaine5/JS/(05) Méthodes/src/js/main.js
+++ Mois 1/semaine5/JS/(05) Méthodes/src/js/main.js
@@ -7,43 +7,35 @@ let zin = "Va manger Number";
 
 console.log(phrase.length);
 
-
 ///### 3. Enlever l'espace à la fin grâce à une méthode
-
-console.log(phrase.trim());
-
+phrase = phrase.trimEnd();
+console.log(phrase);
 
 ///### 4. Reafficher la longueur de la phrase
 
 console.log(phrase.length);
 
-
 ///### 5. Afficher uniquement la dernière lettre de la phrase
-
 console.log(phrase.substr(20));
-
+console.log(phrase.charAt(phrase.length-1));
 
 ///### 6. Enlever "Bonjour" de la phrase
 
 let phrase2 = phrase.replace('Bonjour',' ');
 console.log(phrase2);
 
-
 ///### 7. Remettre "Bonjour" au debut de la phrase grâce à une méthode
 
 let phrase3 = phrase2.replace(' ', 'Bonjour');
 console.log(phrase3);
 
-
 ///### 8. Supprimer toute la phrase sauf "Bonjour" grâce à une méthode
-let phrase4 = phrase3.replace('tout le monde', ' ');
+let phrase4 = phrase3.substr(0,7);
 console.log(phrase4);
-
 
 ///### 9. Remplacer dans la phrase le mot "number" par le mot "string"
 let zin2 = zin.replace('Number', 'string');
 console.log(zin2);
-
 
 ///### 10. Bonus : Donner un chiffre aléatoire
 let zin3 = zin2 + Math.random();
