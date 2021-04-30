bbeb0dc266002ce214bca63efbc0b85a1a7c3284
Seifyuuu <elgayas@hotmail.com>
2021-04-30 10:49:21 +0200

    exo 2

---
+++ Mois 1/semaine5/JS/(02) - Les variables/src/js/main.js
@@ -0,0 +1,58 @@
+// Exo 1 
+let ma_phrase = "j'aime les tartines et j'en mange";
+console.log(ma_phrase);
+
+let jours = "4 par jours";
+console.log(jours);
+console.log(ma_phrase + " " + jours);
+
+let chiffre = 5;
+let nombre = 35; 
+console.log(chiffre + nombre);
+
+
+// let myConcat = (ma_phrase + " " + chiffre);
+// console.log(myConcat); 
+console.log(ma_phrase + " " + chiffre);
+
+
+
+
+// Exo 2 
+let ma_phrase2 = "Salut tout le monde";
+console.log(ma_phrase2);
+let bye = "Au revoir !";
+console.log(bye);
+
+ma_phrase2 = bye;
+console.log(ma_phrase2); 
+
+
+// Exo 3 
+let number = 4; // type int 
+let getal = "6"; // type string ""
+
+console.log(number > getal); // plus grand
+console.log(number < getal); // plus petit 
+
+console.log(number == getal); // égal en valeur 
+console.log(number != getal); // diifférent en valeur 
+
+console.log(number === getal); // égal en valeur & type  
+console.log(number !== getal); // différent en valeur & type 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
