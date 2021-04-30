918e2f82d92f878c68b386987d11dee396053cf9
Seifyuuu <elgayas@hotmail.com>
2021-04-30 11:10:00 +0200

    Correction

--- Mois 1/semaine5/JS/(01) - Les variables/src/js/main.js
+++ Mois 1/semaine5/JS/(01) - Les variables/src/js/main.js
@@ -1,5 +1,11 @@
 let x = "John Doe";
 console.log(x);
-
 let y = "8";
 console.log(y);
+
+console.log("Je m'appelle " + x + " et j'ai " + y + " ans");
+
+
+let nom =  prompt("Tu t'appelles comment ?");
+alert("Tu t'appelles " + nom);
+
