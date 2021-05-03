4e172484911f1d62380adb556dccecf40663857b
Seifyuuu <elgayas@hotmail.com>
2021-05-03 11:57:30 +0200

    exo2

---
+++ Mois 2/semaine6/JS/(02) - Arrays.exercices/src/js/main.js
@@ -0,0 +1,17 @@
+//## Créer une variable qui reçois un array avec 3 éléments -> ex: coding 11, coding 12, coding 13
+let test = ["Banane", "Fraise", "Avocat"];
+
+
+//## Afficher les éléments de l'array
+console.log(test);
+
+//## Afficher le nombre d'éléments de l'array
+console.log(test.length);
+
+
+//## Afficher les éléments de l'array 1 par 1
+console.log(test.join(" - "));
+
+console.log(test[0]);
+console.log(test[1]);
+console.log(test[2]);
