1d41be0e18dabd3344cfde89b6f3a7079d1aae71
Seifyuuu <elgayas@hotmail.com>
2021-05-05 22:30:29 +0200

    exo6

---
+++ Mois 2/semaine6/(06) - Conditions2.exercices/src/js/main.js
@@ -0,0 +1,59 @@
+// // Exo 1 
+// let nombre1 = prompt("Nombre 1");
+// let nombre2 = prompt("Nombre 2");
+
+// if(nombre1 == nombre2){
+//     alert(`C'est égal ? Réponse = ${nombre1 == nombre2}` )
+// }
+
+
+
+// Exo 2 
+// let nombre3 = prompt("Nombre 3");
+// let nombre4 = prompt("Nombre 4");
+
+// if(nombre3 > nombre4){
+//     alert(`La première valeur est plus grande la deuxième ? Réponse = ${nombre3 > nombre4}` )
+// }
+
+
+
+// Exo 3 
+// let nombre5 = prompt("Nombre 5");
+// let nombre6 = prompt("Nombre 6");
+// let nombre7 = prompt("Nombre 7");
+
+// if((nombre5 + nombre6) > nombre7){
+//     alert(`Les deux premières valeurs additionnées sont plus grande que la valeur individuelle ? ${(nombre5 + nombre6) >nombre7}`)
+// }
+
+
+
+// Exo 4 
+// let question = prompt("met ta phrase");
+// let estimation = prompt("estimation longueur phrase");
+// let longueur = question.length;
+// let diff = (longueur - estimation);
+
+// if(estimation == longueur){
+//     alert("respect");
+// }
+
+// else if(estimation != longueur){
+//     alert(`faux, la différence est de ${diff}`);
+// }
+
+
+// Exo 5 
+
+
+let tonNom = prompt("ton nom");
+if (tonNom == ""){
+    prompt("TON NOM")
+};
+
+
+
+// Exo 6
+
+
