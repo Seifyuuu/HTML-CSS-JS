fb5cce4f85c92609a57b541de0c90bd431a22356
Seifyuuu <elgayas@hotmail.com>
2021-05-06 16:39:09 +0200

    ajout correction exo 6

---
+++ Mois 2/semaine6/(07) - Switch.exercices/src/js/main.js
@@ -0,0 +1,28 @@
+// Exo 1
+
+// Exo 2
+let temps = prompt("Quel temps fait t-il ? (pluie/orage/nuage/soleil/neige)");
+switch (temps) {
+  case "pluie":
+    alert("Je prends un parapluie");
+    break;
+  case "orage":
+    alert("J'installe un paratonnerre");
+    break;
+  case "nuage":
+    alert("Je souffle les nuages");
+    break;
+  case "soleil":
+    alert("Je bronze");
+    break;
+  case "neige":
+    alert("Vive la neige");
+    break;
+  default:
+    alert("écrit bien toi aussi");
+    break;
+}
+
+
+// Exo 3 
+
