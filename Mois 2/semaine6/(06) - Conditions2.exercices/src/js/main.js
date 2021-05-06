5b5b1cb87e81077635bcba8af855e07c3244b92d
Seifyuuu <elgayas@hotmail.com>
2021-05-06 14:16:00 +0200

    exo 6 fini

--- Mois 2/semaine6/(06) - Conditions2.exercices/src/js/main.js
+++ Mois 2/semaine6/(06) - Conditions2.exercices/src/js/main.js
@@ -1,4 +1,4 @@
-// // Exo 1 
+// Exo 1
 // let nombre1 = prompt("Nombre 1");
 // let nombre2 = prompt("Nombre 2");
 
@@ -6,9 +6,7 @@
 //     alert(`C'est égal ? Réponse = ${nombre1 == nombre2}` )
 // }
 
-
-
-// Exo 2 
+// Exo 2
 // let nombre3 = prompt("Nombre 3");
 // let nombre4 = prompt("Nombre 4");
 
@@ -16,9 +14,7 @@
 //     alert(`La première valeur est plus grande la deuxième ? Réponse = ${nombre3 > nombre4}` )
 // }
 
-
-
-// Exo 3 
+// Exo 3
 // let nombre5 = prompt("Nombre 5");
 // let nombre6 = prompt("Nombre 6");
 // let nombre7 = prompt("Nombre 7");
@@ -27,9 +23,7 @@
 //     alert(`Les deux premières valeurs additionnées sont plus grande que la valeur individuelle ? ${(nombre5 + nombre6) >nombre7}`)
 // }
 
-
-
-// Exo 4 
+// Exo 4
 // let question = prompt("met ta phrase");
 // let estimation = prompt("estimation longueur phrase");
 // let longueur = question.length;
@@ -43,17 +37,109 @@
 //     alert(`faux, la différence est de ${diff}`);
 // }
 
+// Exo 5
+// let tonNom = prompt("ton nom");
 
-// Exo 5 
+// if (tonNom == "") {
+//  alert("Veuillez remplir tout les champs");
+//     tonNom = prompt("TON NOM");
+// }
 
-
-let tonNom = prompt("ton nom");
-if (tonNom == ""){
-    prompt("TON NOM")
-};
-
-
+// alert("Bonjour " + tonNom);
 
 // Exo 6
+// let abonnement = confirm("Voulez-vous vous abonner ?");
+
+// if (abonnement == true) {
+//   abonnementC = prompt("Abonnement normal ou luxe ?");
+
+//   if (abonnementC == "") {
+//     verif0 = confirm("Veuillez remplir les champs");
+//     if (verif0 == true) {
+//       abonnementC = prompt("Abonnement normal ou luxe?");
+//       if (abonnementC == "normal") {
+//         alert("Vous êtes un abonné normal");
+//       }
+//       if (abonnementC == "luxe") {
+//         alert("Vous êtes un abonné luxe");
+//       }
+//     }
+//   } else if (abonnementC = "luxe") {
+//     alert("abonné de luxe");
+//   } else if (abonnementC = "normal") {
+//     alert("abonné normal");
+//   }
+// }
+
+// if (abonnement == false) {
+//   verif = confirm("Etes-vous sûr ?");
+//   if (verif == false) {
+//     abonnementC = prompt("Abonnement normal ou luxe ?");
+//     if (abonnementC == "") {
+//       verif0 = confirm("Veuillez remplir les champs");
+//       if (verif0 == true) {
+//         abonnementC = prompt("Abonnement normal ou luxe?");
+//         if (abonnementC == "normal") {
+//           alert("Vous êtes un abonné normal");
+//         }
+//         if (abonnementC == "luxe") {
+//           alert("Vous êtes un abonné luxe");
+//         }
+//       }
+//     } else if (abonnementC = "luxe") {
+//       alert("abonné de luxe");
+//     } else if (abonnementC = "normal") {
+//       alert("abonné normal");
+//     }
+//   } else {
+//     alert("Au revoir !");
+//   }
+// }
+
+// Exo 7
+
+// let question1 = prompt("1 + 1");
+// let question2 = prompt("3 + 3");
+
+// let reponse1 = 2;
+// let reponse2 = 6;
+
+
+// if (question1 == reponse1 && question2 == reponse2) {
+//   alert("2/2");
+// } else if (
+//   (question1 == reponse1 && question2 != reponse2) ||
+//   (question1 != reponse1 && question2 == reponse2)
+// ) {
+//   alert("1/2");
+// } else if (question1 != reponse1 && question2 != reponse2) {
+//   alert("0/2");
+// }
+
+
+// Exo 8 
+
+// let age = prompt("Tu as quel âge ?");
+// if (age == ""){
+//     alert("remplissez le champ svp")
+// }
+// if (age < 1 && age != "")  {
+//     alert("vous êtes pas nés")
+// }
+// else if (age >= 18 && age != "") {
+//     alert("Vous pouvez réserver !");
+//     if (age == 18) {
+//         alert("Mais c'est juste !")
+//     }
+//     if (age >= 100) {
+//         alert(" Mais ce n'est pas pour les sensibles")
+//     }
+// }
+// else(alert("Trop jeune !"))
+
+
+
+
+
 
 
