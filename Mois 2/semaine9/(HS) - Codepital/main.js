2452c3770b07d45430c70425e294781079e53e45
Seifyuuu <elgayas@hotmail.com>
2021-05-26 10:09:33 +0200

    push2

--- Mois 2/semaine9/(HS) - Codepital/main.js
+++ Mois 2/semaine9/(HS) - Codepital/main.js
@@ -14,6 +14,13 @@ class Diag{
     }
 }
 
+let diags = []
+diags.push(new Diag("mal indenté", "ctrl+maj+f"));
+diags.push(new Diag("unsave", "saveOnFocusChange"));
+diags.push(new Diag("404", "CheckLinkRelation"));
+diags.push(new Diag("azmatique", "Ventoline"));
+diags.push(new Diag("syntaxError", "f12+doc"));
+
 // ### Tarif des traitements
 // |Traitement|prix|
 // |---|---|
@@ -30,7 +37,12 @@ class Cure{
     }
 }​
 
-
+let cures = [];
+cures.push(new cure("ctrl+maj+f", 60));
+cures.push(new cure("saveOnFocusChange", 100));
+cures.push(new cure("CheckLinkRelation", 35));
+cures.push(new cure("Ventoline", 40));
+cures.push(new cure("f12+doc", 20));
 
 // ## La pharmacie
 // >Les patients iront par après à la pharmacie et recevront leur traitement s'ils ont assez d'argent. Dans le cas ou ils ont assez d'argent ils seront alors en bonne santé sinon ils seront mort et il faudra alors les pousser dans un cimetière.
