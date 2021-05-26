1511393f6b6197788be772301747de41c6d0bf28
Seifyuuu <elgayas@hotmail.com>
2021-05-26 11:34:53 +0200

    push3

--- Mois 2/semaine9/(HS) - Codepital/class.js
+++ Mois 2/semaine9/(HS) - Codepital/class.js
@@ -1,4 +1,31 @@
-export {patients, docteur}
+export {patients, docteur, diags, pharmacie, cimetiere}
+
+// ### Grille des diagnostiques
+// |maladie|traitement|
+// |---|---|
+// |mal indenté|`ctrl+maj+f`|
+// |unsave|`saveOnFocusChange`|
+// |404|`CheckLinkRelation`|
+// |azmatique|`Ventoline`|
+// |syntaxError|`f12+doc`|
+
+
+class Diag{
+    constructor(disease, cure) {
+        this.disease = disease;
+        this.cure = cure;
+    }
+}
+
+let diags = []
+diags.push(new Diag("mal indenté", "ctrl+maj+f"));
+diags.push(new Diag("unsave", "saveOnFocusChange"));
+diags.push(new Diag("404", "CheckLinkRelation"));
+diags.push(new Diag("azmatique", "Ventoline"));
+diags.push(new Diag("syntaxError", "f12+doc"));
+
+
+
 // ## Description des patients
 // >les malades ont un nom, une maladie, de l'argent, une poche, un état de santé,ils savent aller à un endroit, prendre un médicamment et payer. Au début, les patients sont dans un salle d'attente. 
 // ​
@@ -19,9 +46,6 @@ class Patient{
         this.bag = bag;
         this.state = state;
         this.cure = cure;
-    }
-    payer(){
-
     }
     goTo(){
 
@@ -49,15 +73,38 @@ patients.push(new Patient("Semicolon", "syntaxError", 60, "none", "sick"));
 let docteur = {
     name : "Dr.House",
     money : "0",
-    cabinet : "[chat]",
+    cabinet : [],
     attente : patients,
-    function patientIn(){
-        
+   
+    patientIn(){
+        this.cabinet.push(this.attente.shift())
+        let lemalade = this.cabinet[0];
+        console.log(`${lemalade.name} est rentré`); 
     },
-    function diagnostique(){
-        
+    diagnostique(){
+        let lemalade = this.cabinet[0];
+        console.log(`${lemalade.name} a le symptome ${lemalade.disease}`);
+        let curelemalade;
+        for (const i of diags) {
+            if (i.disease == lemalade.disease) {
+                curelemalade = i.cure;
+            }
+        }
+        console.log(`Le traitement de ${lemalade.name} est ${curelemalade}`);
+        lemalade.money = lemalade.money - 50;
+        console.log(`${lemalade.name} a payé 50€`);
     },
-    function patientOut(){
-        
+    patientOut(){
+        console.log(`Le patient est parti`);
+        pharmacie.push(this.cabinet.shift())
+        console.log(`${pharmacie[0].name} est arrivé à la pharmacie`);
     }
 }
+
+
+// ## La pharmacie
+// >Les patients iront par après à la pharmacie et recevront leur traitement s'ils ont assez d'argent. Dans le cas ou ils ont assez d'argent ils seront alors en bonne santé sinon ils seront mort et il faudra alors les pousser dans un cimetière.
+
+let pharmacie = [];
+let cimetiere = [];
+
--- Mois 2/semaine9/(HS) - Codepital/main.js
+++ Mois 2/semaine9/(HS) - Codepital/main.js
@@ -1,26 +1,4 @@
-import { patients, docteur } from "./class.js";
-// ### Grille des diagnostiques
-// |maladie|traitement|
-// |---|---|
-// |mal indenté|`ctrl+maj+f`|
-// |unsave|`saveOnFocusChange`|
-// |404|`CheckLinkRelation`|
-// |azmatique|`Ventoline`|
-// |syntaxError|`f12+doc`|
-
-class Diag{
-    constructor(disease, cure) {
-        this.disease = disease;
-        this.cure = cure;
-    }
-}
-
-let diags = []
-diags.push(new Diag("mal indenté", "ctrl+maj+f"));
-diags.push(new Diag("unsave", "saveOnFocusChange"));
-diags.push(new Diag("404", "CheckLinkRelation"));
-diags.push(new Diag("azmatique", "Ventoline"));
-diags.push(new Diag("syntaxError", "f12+doc"));
+import { patients, docteur, diags, pharmacie, cimetiere } from "./class.js";
 
 // ### Tarif des traitements
 // |Traitement|prix|
@@ -36,21 +14,22 @@ class Cure{
         this.cure = cure;
         this.price = price;
     }
-}​
+}
 
 let cures = [];
-cures.push(new cure("ctrl+maj+f", 60));
-cures.push(new cure("saveOnFocusChange", 100));
-cures.push(new cure("CheckLinkRelation", 35));
-cures.push(new cure("Ventoline", 40));
-cures.push(new cure("f12+doc", 20));
-
-// ## La pharmacie
-// >Les patients iront par après à la pharmacie et recevront leur traitement s'ils ont assez d'argent. Dans le cas ou ils ont assez d'argent ils seront alors en bonne santé sinon ils seront mort et il faudra alors les pousser dans un cimetière.
-
-let pharmacie = [];
-let cimetiere = [];
+cures.push(new Cure("ctrl+maj+f", 60));
+cures.push(new Cure("saveOnFocusChange", 100));
+cures.push(new Cure("CheckLinkRelation", 35));
+cures.push(new Cure("Ventoline", 40));
+cures.push(new Cure("f12+doc", 20));
 
 
 
 
+//
+
+
+console.log(`Dans la salle d'attente il y'a ${docteur.attente.length} personnes`);
+docteur.patientIn()
+docteur.diagnostique()
+docteur.patientOut()
