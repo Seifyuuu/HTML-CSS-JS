fa8f900085e6a827930223fc747c0477e7fa4d10
Seifyuuu <elgayas@hotmail.com>
2021-05-26 14:50:38 +0200

    finito

--- Mois 2/semaine9/(HS) - Codepital/main.js
+++ Mois 2/semaine9/(HS) - Codepital/main.js
@@ -6,14 +6,6 @@ import { patients, docteur, diags, pharmacie, cimetiere, cures,  } from "./class
 
 
 
-
-
-
-
-
-
-
-
 while (docteur.attente.length > 0) {
 console.log("Miaou");
 console.log(`Dans la salle d'attente il y'a ${docteur.attente.length} personnes`);
